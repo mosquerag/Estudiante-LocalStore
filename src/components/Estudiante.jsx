dabbe40c0f5c9df08ee28025a96a2fbe8bdda3aa
mosquerag <120346662+mosquerag@users.noreply.github.com>
2024-08-12 14:41:03 -0500

    primer commit

--- src/components/Estudiante.jsx
+++ src/components/Estudiante.jsx
@@ -5,8 +5,10 @@ export const Estudiante = ({ estudiante, borrar, setEstudiante}) => {
     return (
         <tr>
             <td>
-                <img src="/src/assets/edit.png"   onClick={() => setEstudiante(estudiante)} />
-                <img src="/src/assets/delete.png"  onClick={() => borrar(id)} />
+                {/* <img src="/src/assets/edit.png"   onClick={() => setEstudiante(estudiante)} />
+                <img src="/src/assets/delete.png"  onClick={() => borrar(id)} /> */}
+                <i className="fa-solid fa-pen-to-square me-2" onClick={() => setEstudiante(estudiante)}></i>
+                <i className="fa-solid fa-trash" onClick={() => borrar(id)} ></i>
             </td>
             <th scope="row">{documento}</th>
             <td>{nombre}</td>
